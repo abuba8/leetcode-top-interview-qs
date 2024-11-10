5f3a2e33aded98caf29daa2246522b9df7bcdbac
abuba8 <abuba831@gmail.com>
2024-11-09 19:35:07 -0500

    updated solutions

--- medium/28.find_first_occurance.py
+++ medium/28.find_first_occurance.py
@@ -1,13 +1,13 @@
-def strStr(haystack, needle):
-    """
-    :type haystack: str
-    :type needle: str
-    :rtype: int
-    """
-    if needle not in haystack: return -1
-    else: return haystack.find(needle)
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle not in haystack: return -1
+        else: return haystack.find(needle)
 
-x = strStr("sadbutsad", "sad")
-print(x)
-x = strStr("leetcode", "leeto")
-print(x)
+obj = Solution()
+print(obj.strStr("sadbutsad", "sad"))
+print(obj.strStr("leetcode", "leeto"))
