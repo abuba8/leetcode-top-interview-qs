5366dbc2eed54420fd5d74c3b30e0d06e0f2490c
abuba8 <abuba831@gmail.com>
2024-11-11 18:00:32 -0500

    updated solutions

--- medium/29.divide_two_ints.py
+++ medium/29.divide_two_ints.py
@@ -1,12 +1,14 @@
-def divide(dividend, divisor):
-    """
-    :type dividend: int
-    :type divisor: int
-    :rtype: int
-    """
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        
+        # print(dividend, divisor)
+        print(int(dividend/divisor))
     
-    # print(dividend, divisor)
-    print(int(dividend/divisor))
-    
-divide(10, 3)
-divide(7, -3)
+obj = Solution()
+print(obj.divide(10, 3))
+print(obj.divide(7, -3))
