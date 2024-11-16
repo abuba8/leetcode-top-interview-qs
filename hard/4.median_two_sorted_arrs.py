6e8f98fb50a1ac799e792fe7e1b1db556480ba6f
abuba8 <abuba831@gmail.com>
2024-11-15 21:02:19 -0500

    updated solutions

--- hard/4.median_two_sorted_arrs.py
+++ hard/4.median_two_sorted_arrs.py
@@ -1,17 +1,20 @@
-def findMedianSortedArrays(nums1, nums2):
-    """
-    :type nums1: List[int]
-    :type nums2: List[int]
-    :rtype: float
-    """
+class Solution(object):
+    def findMedianSortedArrays(nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
 
-    final_nums = sorted(nums1 + nums2)
-    if len(final_nums) % 2 == 0:
-        mid_index_1 = len(final_nums) // 2 - 1
-        mid_index_2 = mid_index_1 + 1
-        print(float((final_nums[mid_index_1]+final_nums[mid_index_2])/2) )
-        return float((final_nums[mid_index_1]+final_nums[mid_index_2])/2) 
-    else:
-        mid_index = len(final_nums) // 2
-        print(float(final_nums[mid_index]))
-        return float(final_nums[mid_index])
+        final_nums = sorted(nums1 + nums2)
+        if len(final_nums) % 2 == 0:
+            mid_index_1 = len(final_nums) // 2 - 1
+            mid_index_2 = mid_index_1 + 1
+            print(float((final_nums[mid_index_1]+final_nums[mid_index_2])/2) )
+            return float((final_nums[mid_index_1]+final_nums[mid_index_2])/2) 
+        else:
+            mid_index = len(final_nums) // 2
+            print(float(final_nums[mid_index]))
+            return float(final_nums[mid_index])
+        
+obj = Solution()
